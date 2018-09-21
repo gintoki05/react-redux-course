fb3d5d24c0a2bd29a1ba5bd80b369de6dd108299
gintoki05 <ajieprastyo@gmail.com>
2018-09-21 16:33:43 +0700

    Add count redux

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -3,16 +3,24 @@ import { createStore } from 'redux';
 const store = createStore((state = { count: 0 }, action) => {
   switch (action.type) {
     case 'INCREMENT':
+      const incrementBy =
+        typeof action.incrementBy === 'number' ? action.incrementBy : 1;
       return {
-        count: state.count + 1
+        count: state.count + incrementBy
       };
     case 'RESET':
       return {
         count: (state.count = 0)
       };
     case 'DECREMENT':
+      const decrementBy =
+        typeof action.decrementBy === 'number' ? action.decrementBy : 1;
       return {
-        count: state.count - 1
+        count: state.count - decrementBy
+      };
+    case 'SET':
+      return {
+        count: action.count
       };
     default:
       return state;
@@ -23,9 +31,12 @@ store.subscribe(() => {
   console.log(store.getState());
 });
 
+// ACTION
+
 // Increment
 store.dispatch({
-  type: 'INCREMENT'
+  type: 'INCREMENT',
+  incrementBy: 5
 });
 
 // Increment
@@ -41,4 +52,15 @@ store.dispatch({
 // Decrement
 store.dispatch({
   type: 'DECREMENT'
+});
+
+store.dispatch({
+  type: 'DECREMENT',
+  decrementBy: 10
+});
+
+// Set
+store.dispatch({
+  type: 'SET',
+  count: 101
 });

d9c053ffd851d346be5f42dd17fcfb42b413d360
gintoki05 <ajieprastyo@gmail.com>
2018-09-21 17:25:20 +0700

    Array destructuring playground

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,31 +1,52 @@
-const person = {
-  name: 'Ajie',
-  age: 21,
-  location: {
-    city: 'Palembang',
-    temp: 30
-  }
-};
+//
+// OBJECT DESTRUCTURING
+//
 
-const { name: firstName = 'Anonymus', age } = person; // Object destructure
-// const name = person.name
-// const age = person.age
+// const person = {
+//   name: 'Ajie',
+//   age: 21,
+//   location: {
+//     city: 'Palembang',
+//     temp: 30
+//   }
+// };
 
-console.log(`${firstName} is ${age}`);
+// const { name: firstName = 'Anonymus', age } = person; // Object destructure
+// // const name = person.name
+// // const age = person.age
 
-const { city, temp: temperature } = person.location; // Object destructure
+// console.log(`${firstName} is ${age}`);
 
-console.log(`${temperature} Celcius in ${city}`);
+// const { city, temp: temperature } = person.location; // Object destructure
+
+// console.log(`${temperature} Celcius in ${city}`);
+
+// // CHALLENGE
+// const book = {
+//   title: 'Ego is the Enemy',
+//   author: '  Ryan liburan',
+//   publisher: {
+//     name: 'Penguin'
+//   }
+// };
+
+// const { name: publisherName = 'Self-Published' } = book.publisher;
+
+// console.log(publisherName);
+
+//
+// ARRAY DESTRUCTURING
+//
+
+const address = ['Jl Merpati', 'Palembang', 'Indonesia', '30000'];
+
+const [street, city = 'Jakarta', state, zip] = address;
+
+console.log(`Kamu di ${city} ${state}`);
 
 // CHALLENGE
-const book = {
-  title: 'Ego is the Enemy',
-  author: '  Ryan liburan',
-  publisher: {
-    name: 'Penguin'
-  }
-};
+const item = ['Coffe (hot)', 'Rp.1000', 'Rp.500', 'Rp.200'];
 
-const { name: publisherName = 'Self-Published' } = book.publisher;
+const [coffe, , harga2] = item;
 
-console.log(publisherName);
+console.log(`${coffe} harganya ${harga2}`);
